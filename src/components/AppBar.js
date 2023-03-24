237d05658330f5e3dd04e5c7421583f214f1eb8b
Roald André Kvarv <45228105+PhenomOfficial@users.noreply.github.com>
2023-03-24 13:36:52 +0100

    changed appbar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Appbar from '@mui/material/Appbar';
+import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
