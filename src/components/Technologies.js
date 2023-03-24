1bac08fcbab364c3028b6cc9c4d5057b90863b68
Roald André Kvarv <45228105+PhenomOfficial@users.noreply.github.com>
2023-03-24 14:54:02 +0100

    test

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,24 +1,23 @@
 import * as React from 'react';
-import HTML5 from '../images/HTML5.svg.png'
-import CSSLogo from '../images/CSS.png'
-import Python from '../images/python.png'
-import JS from '../images/JS.png'
-import MySQL from '../images/mysql.png'
-import PHP from '../images/PHP.png'
-import Java from '../images/java.png'
-import { Grid } from '@mui/material';
-
+// import HTML5 from '../images/HTML5.svg.png'
+// import CSSLogo from '../images/CSS.png'
+// import Python from '../images/python.png'
+// import JS from '../images/JS.png'
+// import MySQL from '../images/mysql.png'
+// import PHP from '../images/PHP.png'
+// import Java from '../images/java.png'
+import {Grid} from "@mui/material";
 
 export default function Myskills() {
     return (
         <Grid container direction="row" justifyContent="center" alignItems="center" spacing={2}>
             <Grid item>
                 <img style={{width:"256px"}}
-                    src={HTML5}
-                    alt={HTML5}
+                    src={require('../images/HTML5.svg.png').default}
+                    alt={"HTML5"}
                 />
             </Grid>
-            <Grid item>
+            {/* <Grid item>
                 <img style={{width:"256px"}}
                     src={CSSLogo}
                     alt={CSSLogo}
@@ -53,7 +52,7 @@ export default function Myskills() {
                      src={Java}
                      alt={Java}
                 />
-            </Grid>
+            </Grid> */}
         </Grid>
     );
 }
