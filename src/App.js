287bad2c754a014d48ccbba1818e6e4d427a83e6
Roald André Kvarv <45228105+PhenomOfficial@users.noreply.github.com>
2023-03-24 13:19:29 +0100

    made portfolio app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import AppBar from './components/Appbar'
+import Portfolio from './components/Portfolio'
+import Myskills from './components/Technologies'
+import Myprograms from './components/Programs'
+import Myframeworks from './components/Frameworks'
+import Biography from "./components/Biography";
+
+import Avatar from "./images/para_background.jpg";
+import {Parallax} from "react-parallax";
+import * as React from "react";
+import {Helmet} from "react-helmet";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <title>Roald André's Portfolio</title>
+        <meta name="description" content="Deverloper portfolio for Roald André Kvarv, made using Java/Springboot and
+        react"/>
+      </Helmet>
+      <AppBar/>
+        <Parallax strength={600} bgImage={Avatar}>
+          <Portfolio/>
+          <div className="content">
+            <div className="text-content"></div>
+          </div>
+        </Parallax>
+        <h1>About Me</h1>
+        <Biography/>
+        <h1>My Skills</h1>
+        <h2>Programming Languages</h2>
+        <Myskills/>
+        <h2>Frameworks</h2>
+        <Myprograms/>
+        <h2>Programs/Applications</h2>
+        <Myframeworks/>
     </div>
   );
 }
