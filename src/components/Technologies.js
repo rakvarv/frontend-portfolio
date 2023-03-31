b342eb428eb8ba78a879a9ac1ae81257c3993908
Roald André Kvarv <45228105+PhenomOfficial@users.noreply.github.com>
2023-03-31 11:23:35 +0200

    adding image rows

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,63 +1,33 @@
 import * as React from 'react';
-// import HTML5 from '../images/HTML5.svg.png'
-// import CSSLogo from '../images/CSS.png'
-// import Python from '../images/python.png'
-// import JS from '../images/JS.png'
-// import MySQL from '../images/mysql.png'
-// import PHP from '../images/PHP.png'
-// import Java from '../images/java.png'
+import HTML5 from '../images/HTML5.svg.png'
+import CSSLogo from '../images/CSS.png'
+import Python from '../images/python.png'
+import JS from '../images/JS.png'
+import MySQL from '../images/mysql.png'
+import PHP from '../images/PHP.png'
+import Java from '../images/java.png'
 // import { Grid } from '@mui/material';
 
 
 export default function Myskills() {
+    const images = [
+        HTML5,
+        CSSLogo,
+        Python,
+        JS,
+        MySQL,
+        PHP,
+        Java
+    ]
+
+    
     return (
-        <p>Cool Tech</p>
-        // <Grid container direction="row" justifyContent="center" alignItems="center" spacing={2}>
-        //     {/* <Grid item>
-        //         <p>This is crazy technology</p>
-        //     </Grid> */}
-        //     {/* <Grid item>
-        //         <img style={{width:"256px"}}
-        //             src={require('../images/CSS.png').default}
-        //             alt={"CSS"}
-        //         />
-        //     </Grid> */}
-        //     {/* <Grid item>
-        //         <img style={{width:"256px"}}
-        //             src={CSSLogo}
-        //             alt={CSSLogo}
-        //         />
-        //     </Grid>
-        //     <Grid item>
-        //         <img style={{width:"256px"}}
-        //              src={Python}
-        //              alt={Python}
-        //         />
-        //     </Grid>
-        //     <Grid item>
-        //         <img style={{width:"256px"}}
-        //              src={JS}
-        //              alt={JS}
-        //         />
-        //     </Grid>
-        //     <Grid item>
-        //         <img style={{width:"256px"}}
-        //              src={MySQL}
-        //              alt={MySQL}
-        //         />
-        //     </Grid>
-        //     <Grid item>
-        //         <img style={{width:"256px"}}
-        //              src={PHP}
-        //              alt={PHP}
-        //         />
-        //     </Grid>
-        //     <Grid item>
-        //         <img style={{width:"256px"}}
-        //              src={Java}
-        //              alt={Java}
-        //         />
-        //     </Grid> */}
-        // </Grid>
+        <div className="image-flexbox">
+        {images.map((imageUrl, index) => (
+            <div key={index} className="image-container">
+            <img src={imageUrl} alt={`Image ${index}`} />
+            </div>
+        ))}
+        </div>
     );
 }
